e8dfa5c02136c37bbf7f1d29a1b7ae01b1e90b2b
Oscar Hogben <oscarhogben@gmail.com>
2022-02-17 20:32:03 +0000

    Make port dynamic

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let topScore = 0;
+let orePrices = {"Korrelite": 1, "Reknite": 2};
 
-app.get('/score', (req, res) => {
-    console.log('Received Score Request!!');
-    res.send(topScore.toString());
+app.get('/starscape/oreprices', (req, res) => {
+    console.log('Received ore prices request');
+    res.send(orePrices);
 });
 
-app.post('/score', (req, res) => {
-    console.log(req.body.Score);
-    topScore = req.body.Score;
-    res.send(topScore.toString());
+app.post('/starscape/oreprices', (req, res) => {
+    let key = req.body.key.toString();
+    if (key == "1lxwIhPKJDVl5YXDQ6XF"); {
+        
+        if (req.body.korrelite != undefined) {
+            orePrices['Korrelite'] = parseFloat(req.body.korrelite.toString());
+        };
+
+        if (req.body.reknite != undefined) {
+            orePrices['Reknite'] = parseFloat(req.body.reknite.toString());
+        };
+        
+    };
+    res.send(201);
 });
 
 app.listen(port, () => {
